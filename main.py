b21031860249a3a11b384ce07386499011ad8fb9
YeongRoYun <58219267+YeongRoYun@users.noreply.github.com>
2023-11-09 10:19:25 +0900

    [feat] done

---
+++ main.py
@@ -0,0 +1,22 @@
+import argparse
+
+from lib.ai.search import SearchAI
+
+
+parser = argparse.ArgumentParser(
+    prog="pyoniverse search engine",
+    description="여러 검색 모델을 앙상블하여 최선의 결과를 반환한다. 모델은 FastAPI를 통해 통신한다.",
+    epilog="created by yyr",
+)
+if __name__ == "__main__":
+    print("make embedding")
+    print("start test")
+    item_search_module = SearchAI(data_path="resource/data/products.json", embedding_paths=True)
+
+    print(item_search_module.search("치즈 김밥"))
+
+    while True:
+        query = input()
+        if query == "x":
+            break
+        print(item_search_module.search(query))
---
+++ requirements.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+pip install tensorflow
+pip install keras
+pip install --no-deps git+https://github.com/haven-jeon/PyKoSpacing.git
+pip install -U sentence-transformers
+pip install rank_bm25
+pip install -r requirements.txt
