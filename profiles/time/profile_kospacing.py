2d13aebd9421913a831f7e67ac39d02beb5c05a1
YeongRoYun <58219267+YeongRoYun@users.noreply.github.com>
2023-11-11 00:52:30 +0900

    [feat] profile sentence embedding

--- profiles/time/profile_kospacing.py
+++ profiles/time/profile_kospacing.py
@@ -1,9 +1,9 @@
 """
 [KoSpacing의 속도 측정]
 이유: 현재 모듈에서 Spacing을 매번 처리하는데, 10,000개 정도의 데이터를 처리하는데 5분정도 걸린다.
-테스트 내용: 100개의 무작위 데이터로 연산을 10회 실시해 평균 속도를 구한다.
-테스트 실행(line_profiler 사용): kernprof -l -v {script}
-테스트 결과: Space.__call__ 자체가 시간이 오래걸린다(30ms/name) -> 10,000개의 데이터로 확장하면 300sec ~ 5분
+테스트 내용: 1000개의 무작위 데이터로 연산을 10회 실시해 평균 속도를 구한다.
+테스트 실행: kernprof -l -v -u 1e-3 profiles/time/profile_kospacing.py > profiles/time/report/profile_kospacing.txt(밀리초)
+테스트 결과: Space.__call__ 자체가 시간이 오래걸린다(26sec/name)
 결론: Spacing 결과를 저장하자
 """
 import json
@@ -33,7 +33,7 @@ def profile_kospace(count: int = 10):
     with open("profiles/resource/products.json", "r") as fd:
         data = json.load(fd)
         for _ in range(count):
-            samples = sample(data, k=100)
+            samples = sample(data, k=1000)
             execute_kospace(samples)
 
 
