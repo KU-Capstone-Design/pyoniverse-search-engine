4967273ef215d04170c88746c2b5054fe5e818eb
YeongRoYun <58219267+YeongRoYun@users.noreply.github.com>
2023-11-20 19:05:27 +0900

    done?

--- lib/ai/search.py
+++ lib/ai/search.py
@@ -1,5 +1,5 @@
 import logging
-from collections import namedtuple
+from dataclasses import dataclass
 from typing import List, Literal
 
 import numpy as np
@@ -13,9 +13,15 @@ from lib.ai.model.search import SearchResponseDto
 from lib.config import get_settings
 
 
+@dataclass(frozen=True)
+class SearchData:
+    id: int
+    name: str
+    score: float
+
+
 class SearchAI:
     __instance = None
-    Data = namedtuple("Data", ["id", "name", "score"])
 
     @classmethod
     def instance(cls):
@@ -37,18 +43,24 @@ class SearchAI:
         self.__version = version
         assert self.__version in {"v1"}
 
+        # TODO : 병렬 성능 테스트
+
+        # self.__pipes: Dict[str, Connection] = {}
+        # self.__processes: Dict[str, Process] = {}
+        # self.__setup_parallel()
+
     def search(self, query: str, limit: float = 0.5) -> SearchResponseDto:
         """
         :param query: 검색어
         :param limit: 반환할 결과의 최소 정확도
         :return: SearchResponseDto
         """
-        data: List[SearchAI.Data] = []
+        data: List[SearchData] = []
         for model in self.__models:
             if model.type == "lexical":
-                data += self.__search_with_lexical(model=model, query=query, limit=limit)
+                data += self._search_with_lexical(model=model, query=query, limit=limit)
             elif model.type == "sentence":
-                data += self.__search_with_sentence(model=model, query=query, limit=limit)
+                data += self._search_with_sentence(model=model, query=query, limit=limit)
             else:
                 self.logger.info(f"{model.type} isn't supported")
         results = self.__ansible(query=query, data=data)
@@ -57,38 +69,38 @@ class SearchAI:
         response = SearchResponseDto(version=self.__version, engine_type="ML", results=[r.id for r in results])
         return response
 
-    def __search_with_sentence(self, model: SearchModel, query: str, limit: float) -> List["SearchAI.Data"]:
+    def _search_with_sentence(self, model: SearchModel, query: str, limit: float) -> List[SearchData]:
         engine: SentenceTransformer = model.engine
         encoded_query = engine.encode(query)
         if norm := np.linalg.norm(encoded_query):
             encoded_query = encoded_query / norm
         else:
             encoded_query = np.zeros_like(encoded_query)
-        result: List[SearchAI.Data] = []
+        result: List[SearchData] = []
         for e in model.embeddings:
             if norm := np.linalg.norm(e.embedding):
                 normalized_embedding = e.embedding / norm
             else:
                 normalized_embedding = np.zeros_like(e.embedding)
             score = np.dot(encoded_query, normalized_embedding)
-            result.append(SearchAI.Data(score=score, id=e.id, name=e.name))
+            result.append(SearchData(score=score, id=e.id, name=e.name))
 
         result = sorted(result, key=(lambda x: x.score), reverse=True)
         result = list(filter(lambda x: x.score >= limit, result))
         return result
 
-    def __search_with_lexical(self, model: SearchModel, query: str, limit: float) -> List["SearchAI.Data"]:
+    def _search_with_lexical(self, model: SearchModel, query: str, limit: float) -> List[SearchData]:
         engine: BM25Okapi = model.engine
         tokenized_query = query.split(" ")
         # corpus 순서대로 스코어 반환
         scores = engine.get_scores(tokenized_query)
-        result: List[SearchAI.Data] = []
+        result: List[SearchData] = []
         for score, e in zip(scores, model.embeddings):
             if score >= limit:
-                result.append(SearchAI.Data(score=score, id=e.id, name=e.name))
+                result.append(SearchData(score=score, id=e.id, name=e.name))
         return sorted(result, key=lambda x: x.score, reverse=True)
 
-    def __ansible(self, query: str, data: List["SearchAI.Data"], limit: float = 0.5) -> List["SearchAI.Data"]:
+    def __ansible(self, query: str, data: List[SearchData], limit: float = 0.5) -> List[SearchData]:
         """
         여러 모델의 검색 결과를 하나로 합쳐 가장 높은 유사도를 찾는다.
         :param results: [{"score": ..., "id": ..., "name": ...}]
@@ -96,8 +108,102 @@ class SearchAI:
         :return: [{"score": ..., "id": ..., "name": ...}]
         """
         scores = self.__cross_encoder.predict([[query, d.name] for d in data])
-        result: List[SearchAI.Data] = []
+        result: List[SearchData] = []
         for d, score in zip(data, scores):
             if score >= limit:
-                result.append(SearchAI.Data(score=score, id=d.id, name=d.name))
+                result.append(SearchData(score=score, id=d.id, name=d.name))
         return sorted(result, key=lambda x: x.score, reverse=True)
+
+    # def __setup_parallel(self):
+    #     self.__processes: Dict[str, Process] = {}
+    #     for model in self.__models:
+    #         model_name = str(model.engine)
+    #         p_conn, c_conn = Pipe()
+    #         if model.type == "lexical":
+    #             self.__processes[model_name] = Process(target=self._search_with_lexical_parallel,
+    #                                                    kwargs={"model": model_name, "conn": c_conn})
+    #         elif model.type == "sentence":
+    #             self.__processes[model_name] = Process(target=self._search_with_sentence_parallel,
+    #                                                    kwargs={"model": model_name, "conn": c_conn})
+    #         else:
+    #             raise RuntimeError(f"Don't support {model.type}")
+    #         self.__pipes[model_name] = p_conn
+    #         self.__processes[model_name].start()
+
+    # def search_parallel(self, query: str, limit: float = 0.5) -> SearchResponseDto:
+    #     """
+    #     :param query: 검색어
+    #     :param limit: 반환할 결과의 최소 정확도
+    #     :return: SearchResponseDto
+    #     """
+    #     data = []
+    #     for model in self.__models:
+    #         self.__pipes[str(model.engine)].send((query, limit))
+    #
+    #     for model in self.__models:
+    #         data += self.__pipes[str(model.engine)].recv()
+    #     results = self.__ansible(query=query, data=data)
+    #     if not results:
+    #         raise HTTPException(status_code=404, detail="Empty Result")
+    #     response = SearchResponseDto(version=self.__version, engine_type="ML", results=[r.id for r in results])
+    #     return response
+    #
+    # def _search_with_sentence_parallel(self, model: str, conn: Connection) -> NoReturn:
+    #     for m in self.__models:
+    #         if str(m.engine) == model:
+    #             model = m
+    #             break
+    #     engine: SentenceTransformer = model.engine
+    #
+    #     while True:
+    #         try:
+    #             query, limit = conn.recv()
+    #         except EOFError:
+    #             self.logger.info("Close connection")
+    #             return
+    #         encoded_query = engine.encode(query)
+    #         if norm := np.linalg.norm(encoded_query):
+    #             encoded_query = encoded_query / norm
+    #         else:
+    #             encoded_query = np.zeros_like(encoded_query)
+    #         result: List[SearchData] = []
+    #         for e in model.embeddings:
+    #             if norm := np.linalg.norm(e.embedding):
+    #                 normalized_embedding = e.embedding / norm
+    #             else:
+    #                 normalized_embedding = np.zeros_like(e.embedding)
+    #             score = np.dot(encoded_query, normalized_embedding)
+    #             result.append(SearchData(score=score, id=e.id, name=e.name))
+    #
+    #         result = sorted(result, key=(lambda x: x.score), reverse=True)
+    #         result = list(filter(lambda x: x.score >= limit, result))
+    #         conn.send(result)
+    #
+    # def _search_with_lexical_parallel(self, model: str, conn: Connection) -> NoReturn:
+    #     for m in self.__models:
+    #         if str(m.engine) == model:
+    #             model = m
+    #             break
+    #     engine: BM25Okapi = model.engine
+    #     while True:
+    #         try:
+    #             query, limit = conn.recv()
+    #         except EOFError:
+    #             self.logger.info("Close connection")
+    #             return
+    #         tokenized_query = query.split(" ")
+    #         # corpus 순서대로 스코어 반환
+    #         scores = engine.get_scores(tokenized_query)
+    #         result: List[SearchData] = []
+    #         for score, e in zip(scores, model.embeddings):
+    #             if score >= limit:
+    #                 result.append(SearchData(score=score, id=e.id, name=e.name))
+    #         result = sorted(result, key=lambda x: x.score, reverse=True)
+    #         conn.send(result)
+    #
+    # def exit(self):
+    #     for conn in self.__pipes.values():
+    #         conn.close()
+    #     for p in self.__processes.values():
+    #         p.terminate()
+    #         p.close()
